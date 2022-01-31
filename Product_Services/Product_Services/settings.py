880b7c3e81e85d02f2afda77e0a7f89982234bd7
Felsy <felsymartin@gmail.com>
2022-01-31 13:23:27 +0530

    Home app_ static file created, static file(root) created,Home page template modified (jinja command used)

--- Product_Services/Product_Services/settings.py
+++ Product_Services/Product_Services/settings.py
@@ -50,11 +50,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'Product_Services.urls'
-
+import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static'),
+]
+STATIC_ROOT=os.path.join(BASE_DIR,'asset')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
