6c41ebf43d8f4c5c23c924f3e077f155995332ff
Felsy <felsymartin@gmail.com>
2022-02-02 12:15:43 +0530

    A register template,  A success & invalid template for Login was also created and connected to our web-page, Created a Database in MySQL - productdb and connected it with Django, also created tables using migrations

--- Product_Services/register/views.py
+++ Product_Services/register/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 def login(request):
     return render(request,'login.html')
+def register(request):
+    return render(request,'register.html')
+def validate(request):
+    uname=request.GET['uname']
+    psw=request.GET['psw']
+    if uname=='felsy' and psw=='felsy@123':
+
+        return render(request,'success.html')
+    else:
+        return render(request,'invalid.html')
