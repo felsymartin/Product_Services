8715d481d9bdc299e5b61379f61a3a6eb9a08466
Felsy <felsymartin@gmail.com>
2022-02-09 14:29:55 +0530

    Offer tag given, Product details:- Id of the product was used for setting url path using GET method. Using JINJA command corresponding edit was given in product details section

--- Product_Services/product/views.py
+++ Product_Services/product/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from home.models import product
 
 
 # Create your views here.
 
 def productdetails(request):
-    return render(request,'product.html')
+
+    pro_id = request.GET['id']
+    obj_id = product.objects.get(id=pro_id)
+    return render(request,'product.html',{'objid':obj_id})
